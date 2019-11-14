9a91fbf58caaaa652b282652b1e61c7c9ea1b686
eureka84 <angelosciarra@ymail.com>
2019-11-14 19:25:59 +0100

    updated fp-ts

--- src/ts/mars_rover_kata/__tests__/ask.test.ts
+++ src/ts/mars_rover_kata/__tests__/ask.test.ts
@@ -1,36 +1,35 @@
 import {ask} from "../ioOps";
-import {Task} from "fp-ts/lib/Task";
+import {task} from "fp-ts/lib/Task";
 
 describe('ask', () => {
 
-	it('should provide the given answer',  (done) => {
+    it('should provide the given answer', (done) => {
         const bddStdin = require('bdd-stdin');
 
         bddStdin("answer");
 
-        let stringTask: Task<string> = ask("A question");
-        stringTask.run()
-			.then((answer) => {
-				expect(answer).toBe("answer");
-				done();
-			});
-	});
+        ask("A question")()
+            .then((answer) => {
+                expect(answer).toBe("answer");
+                done();
+            });
+    });
 
-	it('should provide the given answers',  (done) => {
-		const bddStdin = require('bdd-stdin');
+    it('should provide the given answers', (done) => {
+        const bddStdin = require('bdd-stdin');
 
-		bddStdin('answer', 'yes');
+        bddStdin('answer', 'yes');
 
-		ask("A question")
-			.chain((answer1) => {
-				expect(answer1).toBe("answer");
-				return ask(`was ${answer1} your answer`);
-			})
-			.run()
-			.then((answer) => {
-				expect(answer).toBe("yes");
-				done();
-			});
-	});
+        task.chain(
+            ask("A question"),
+            (answer1) => {
+                expect(answer1).toBe("answer");
+                return ask(`was ${answer1} your answer`);
+            }
+        )().then((answer) => {
+            expect(answer).toBe("yes");
+            done();
+        });
+    });
 
 });
--- src/ts/mars_rover_kata/ioOps.ts
+++ src/ts/mars_rover_kata/ioOps.ts
@@ -1,21 +1,20 @@
-import {Task} from 'fp-ts/lib/Task'
+import {task, Task} from 'fp-ts/lib/Task'
 
 /** reads from standard input */
-export const reads: Task<string> = new Task(
-	() => new Promise(resolve => {
-		process.stdin.once('data', function (data) {
-			resolve(data.toString().trim());
-		});
-	})
-);
+export const reads: Task<string> =
+    () => new Promise(resolve => {
+        process.stdin.once('data', function (data) {
+            resolve(data.toString().trim());
+        });
+    });
+
 
 /** writes to standard output */
-export const puts = (message: string): Task<void> => new Task(
-	() => new Promise(resolve => {
-		process.stdout.write(`${message}\n`);
-		resolve();
-	})
-);
+export const puts = (message: string): Task<void> =>
+    () => new Promise(resolve => {
+        process.stdout.write(`${message}\n`);
+        resolve();
+    });
 
 export const ask: (message: string) => Task<string> =
-	(message) => puts(message).chain(() => reads);
+    (message) => task.chain(puts(message), () => reads);

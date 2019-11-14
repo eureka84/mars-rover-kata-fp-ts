7de4af4fd81775e5f9803f9d3cf45182568c2411
eureka84 <angelosciarra@ymail.com>
2019-11-14 23:39:34 +0100

    rewrote using pipe and pipeable

--- src/ts/mars_rover_kata/game.ts
+++ src/ts/mars_rover_kata/game.ts
@@ -1,33 +1,19 @@
-import {task, Task} from "fp-ts/lib/Task";
+import {Task, task} from "fp-ts/lib/Task";
 import {handleCommands} from "./gamePlay";
-import {makeRover, Planet, planetWithObstacles, Result, Rover} from "./data";
+import {Command, makeRover, planetWithObstacles, Position} from "./data";
 import {display, readCommands, readObstacles, readPlanet, readPosition, welcome} from "./gameInteractions";
+import {pipe, pipeable} from "fp-ts/lib/pipeable";
 
-const addObstacles = (planet: Planet) => task.map(readObstacles(), (obs) => planetWithObstacles(planet, obs));
 
-const readPlanetWithObstacles: () => Task<Planet> = () => task.chain(readPlanet(), addObstacles);
-
-const readRover: (planet: Planet) => Task<Rover> =
-    (planet: Planet) => task.map(readPosition(), (pos) => makeRover(pos, planet));
-
-const readInitialSetup: () => Task<Rover> = () => task.chain(readPlanetWithObstacles(), readRover);
-
-const moveRover: (rover: Rover) => Task<Result> =
-    (rover: Rover) => task.map(readCommands(), cs => handleCommands(rover, cs));
-
-const computeResult: () => Task<Result> = () => task.chain(
-    readInitialSetup(),
-    moveRover
+const _ = pipeable(task);
+const game: Task<void> = pipe(
+    welcome(),
+    _.chain(readPlanet),
+    _.chain(planet => _.map((obs: Position[]) => planetWithObstacles(planet, obs))(readObstacles())),
+    _.chain(planet => _.map((pos: Position) => makeRover(pos, planet))(readPosition())),
+    _.chain(rover => _.map((cs: Command[]) => handleCommands(rover, cs))(readCommands())),
+    _.chain(display)
 );
-
-const game: Task<void> =
-    task.chain(
-        welcome(),
-        () => task.chain(
-            computeResult(),
-            display
-        )
-    );
 
 export function run(): Promise<void> {
     return game();

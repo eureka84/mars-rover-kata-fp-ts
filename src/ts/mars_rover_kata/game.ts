b0e78490c9a4043bf8d760dd0b3057d42fd95ba1
eureka84 <angelosciarra@ymail.com>
2019-11-14 08:48:34 +0100

    removed implementation

--- src/ts/mars_rover_kata/game.ts
+++ src/ts/mars_rover_kata/game.ts
@@ -1,16 +1,3 @@
-import {Task} from "fp-ts/lib/Task";
-import {handleCommands} from "./gamePlay";
-import {makeRover, planetWithObstacles} from "./data";
-import {display, readCommands, readObstacles, readPlanet, readPosition, welcome} from "./gameIntercations";
-
-const game: Task<void> =
-    welcome()
-        .chain(readPlanet)
-        .chain((planet) => readObstacles().map((obs) => planetWithObstacles(planet, obs)))
-        .chain((planet) => readPosition().map((pos) => makeRover(pos, planet)))
-        .chain((rover) => readCommands().map(cs => handleCommands(rover, cs)))
-        .chain((result) => display(result));
-
 export function run(): Promise<void> {
-   return game.run();
+    throw "TODO!";
 }
